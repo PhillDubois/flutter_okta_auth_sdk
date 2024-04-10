b57ee8c5269d62d85ace5a215f67385e86d99441
Louis Philippe DUBOIS (OSV) <louis.dubois.osv@fedex.com>
2024-04-10 09:08:22 -0300

    wip

--- android/src/main/kotlin/com/bluestork/flutter_okta_auth_sdk/okta/operations/ClearTokens.kt
+++ android/src/main/kotlin/com/bluestork/flutter_okta_auth_sdk/okta/operations/ClearTokens.kt
@@ -5,7 +5,7 @@ import com.bluestork.flutter_okta_auth_sdk.okta.entities.PendingOperation
 
 fun clearTokens() {
     OktaClient.getWebClient().sessionClient.clear()
-    OktaClient.getAuthClient().sessionClient.clear()
+//    OktaClient.getAuthClient().sessionClient.clear()
 
     PendingOperation.success(true)
 }
--- android/src/main/kotlin/com/bluestork/flutter_okta_auth_sdk/okta/operations/SignOut.kt
+++ android/src/main/kotlin/com/bluestork/flutter_okta_auth_sdk/okta/operations/SignOut.kt
@@ -2,8 +2,29 @@ package com.bluestork.flutter_okta_auth_sdk.okta.operations
 
 import android.app.Activity
 import com.bluestork.flutter_okta_auth_sdk.okta.entities.OktaClient
+import com.okta.oidc.RequestCallback
+import com.okta.oidc.Tokens
+import com.okta.oidc.util.AuthorizationException
+
 
 fun signOut(activity: Activity) {
     registerCallback(activity)
     OktaClient.getWebClient().signOutOfOkta(activity)
+
+//    try {
+//        val token: Tokens = OktaClient.getWebClient().sessionClient.tokens
+//        OktaClient.getWebClient().sessionClient.revokeToken(token.refreshToken,
+//                object : RequestCallback<Boolean, AuthorizationException?> {
+//                    override fun onSuccess(result: Boolean) {
+//                        //handle result
+//                    }
+//
+//                    override fun onError(error: String, exception: AuthorizationException?) {
+//                        //handle request error
+//                    }
+//                })
+//    } catch (e: AuthorizationException) {
+//        //handle error
+//    }
+    OktaClient.getWebClient().sessionClient.clear()
 }
